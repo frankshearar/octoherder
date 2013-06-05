11f8b6e265cb0a379d8979059d4b7c05ff182ee2
Frank Shearar <frank@lshift.net>
2013-06-05 15:10:29 +0100

    Now we can read milestones.

--- lib/octoherder/configuration.rb
+++ lib/octoherder/configuration.rb
@@ -4,8 +4,19 @@ require 'yaml'
 module OctoHerder
   Configuration = Hamsterdam::Struct.define(:milestones)
   class Configuration
-    def self.read file
-      Configuration.new
+    def self.read_file path
+      File.open(path.to_s, "r") { |f| self.read_string f.read }
+    end
+
+    def self.read_string source
+      data = if source.empty? then
+               {}
+             else
+               y = YAML.load(source)
+             end
+
+      milestones = data.fetch('milestones', [])
+      Configuration.new milestones: milestones
     end
   end
 end
--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -4,17 +4,28 @@ require 'octoherder/configuration'
 
 module OctoHerder
   describe Configuration do
+    let (:conf_file) { (Pathname.new(__FILE__).parent + "data/sample.yml").to_s }
+
     it "should be instantiable" do
       Configuration.new
     end
 
     it "can read in a configuration file" do
-      Configuration.read "data/sample.yml"
+      Configuration.read_file conf_file
+    end
+
+    it "can read in a string" do
+      Configuration.read_string File.open(conf_file, "r") { |f| f.read }
+    end
+
+    it "should handle empty input" do
+      conf = Configuration.read_string ""
+      expect(conf.milestones).to be_empty
     end
 
     context "with sample.yml" do
-      let(:conf) { Configuration.read "data/sample.yml" }
-      let(:source) { YAML.read_file "data/sample.yml" }
+      let(:conf) { Configuration.read_file conf_file }
+      let(:source) { YAML.load_file conf_file }
       it "can read in the milestones" do
         expect(conf.milestones.count).to equal(source['milestones'].count)
       end
