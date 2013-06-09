6ff3d1df915dbc2086937ac5440e4ad8dcef6b54
Frank Shearar <frank.shearar@gmail.com>
2013-06-09 22:02:56 +0100

    We don't use the usage outside of the class anymore, using a hard-coded test literal instead.

--- lib/octoherder/cli.rb
+++ lib/octoherder/cli.rb
@@ -4,8 +4,10 @@ require 'trollop'
 
 module OctoHerder
   class CLI
-    def self.usage
-      <<-HELP
+    def self.run(args, octokit_client)
+      p = Trollop::Parser.new do
+        version OctoHerder::VERSION
+        banner <<-HELP
 OctoHerder helps you manage your multi-repository project.
 
 Usage:
@@ -13,12 +15,6 @@ Usage:
     octoherder [options]
 where [options] are:
 HELP
-    end
-
-    def self.run(args)
-      p = Trollop::Parser.new do
-        version OctoHerder::VERSION
-        banner CLI.usage
         opt :'input-file', 'Path to the canonical project setup', :short => 'f'
         opt :repo, 'Name of the master repository', :short => 'r'
         opt :'output-file', 'Path to the file that will contain the canonical project setup', :short => 'o'
